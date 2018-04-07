5dcfa0b8387feb016a0f4018caecd70a1057cee4
Valentyn Holovach <valentyn@holova.ch>
2018-04-07 16:11:22 +0300

    removed unneeded blocks

--- strapi/auth.js
+++ strapi/auth.js
@@ -1,12 +1,16 @@
 module.exports = function(RED) {
-      function OnFirebase(config) {
+      function OnStrapi(config) {
+            var request = require('request');
             
             RED.nodes.createNode(this);
+
+            var context = this.context;
             
             var node = this;
 
             var identifier = this.credentials.identifier;
             var password = this.credentials.password;
+            var url = this.credentials.url;
 
 
             node.on('input', function(msg) {
@@ -14,21 +18,20 @@ module.exports = function(RED) {
             });
 
             function strapiAuth( msg ) {
-                  firebase.app.auth().signInWithEmailAndPassword(config.user, config.password).then(function( result ) {
-                        node.log("Session Opened...");
-                        result.getIdToken().then(function(token) {
-                              node.error(token);
-                              msg.topic = {"idToken" : token, "auth" : true};
-                              node.send(msg);
-                        });
+                  request({
+                        identifier: identifier,
+                        password: password,
+                        url: url,
+                        json: true,
+                        method: 'POST'
+                  }).then(function( result ) {
+                        context.strapiAuth = result.auth;
                   }, function(error) {
-                        var errorCode = error.code;
-                        var errorMessage = error.message;
-                        node.error("Errors Open Auth : " + errorCode + " " + errorMessage);
-                        msg.topic = {"idToken" : null, "auth" : false};
+                        node.error("An error occured: " + error);
+                        msg.topic = {"user" : null, "token" : null};
                         node.send(msg);
                   });
             }
       }
-      RED.nodes.registerType("auth", OnFirebase);
+      RED.nodes.registerType("auth", OnStrapi);
 }
